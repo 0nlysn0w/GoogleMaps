80195dd4f964bd90bf0f9d5108d72cf9ae3e514b
Joost Stam <joost.stam@gmail.com>
2018-03-28 23:09:51 +0200

    Woah. It can show multiple search actions at once on the map

--- MapsFunctions.js
+++ MapsFunctions.js
@@ -1,4 +1,5 @@
 ﻿var map;
+var infowindow;
 function initMap() {
 
 	var tirStyle =	[
@@ -61,25 +62,60 @@ function initMap() {
 		}
 	  ]
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: { lat: 51.9182711, lng: 4.4791135 },
-		zoom: 12
-		//styles: tirStyle,
-	});
-	var clickHandler = new ClickEventHAndler(map);
-
-	// TODO: Set labels from POI type
-	var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-	// TODO: Add info for hover
-	// TODO: Make clickable for navigation
-	var markers = locations.map(function (location, i) {
-		return new google.maps.Marker({
-			position: location,
-			label: labels[i % labels.length]
-		});
+		center: { lat: 51.918404, lng: 4.4811438 },
+		zoom: 17,
+		styles: tirStyle
 	});
 
-	//var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'img/m' });
+	infowindow = new google.maps.InfoWindow();
+	
+	var diffPlaces = ["McDonald's", "KFC"];
+
+	diffPlaces.forEach(place => {
+		var service = new google.maps.places.PlacesService(map);
+		service.nearbySearch({
+			location: {lat: 51.918404, lng: 4.4811438},
+			radius: 500,
+			keyword: place
+		}, callback);
+	
+	});
+
+	
+	// // TODO: Set labels from POI type
+	// var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+	// // TODO: Add info for hover
+	// // TODO: Make clickable for navigation
+	// var markers = locations.map(function (location, i) {
+	// 	return new google.maps.Marker({
+	// 		position: location,
+	// 		label: labels[i % labels.length]
+	// 	});
+	// });
+
+	// //var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'img/m' });
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
 
 // TODO: Get locations from DB
